4a8f8ae3da9a33e05265c590bfee86fbcd9b9b5a
Yaniv Elimor <yaniv.elimor@gmail.com>
2016-10-29 20:05:49 +0300

    add option to use ajax only (disables websockets)

--- src/questions-client.js
+++ src/questions-client.js
@@ -2,24 +2,31 @@ import config from './config';
 import Faye from 'faye';
 import { isFunction } from './utils';
 
-// TODO (yaniv): onQuestionApproved and onQuestionsReceived should only be received on a specific channel for the user client
+// TODO (yaniv):
+// onQuestionApproved and onQuestionsReceived should only be received on a specific channel for the user client
 
 class QuestionsClient {
   constructor(options) {
     this.apiUrl = options.url;
-    this.client = Faye.Client(this.apiUrl, { retry: config.retry });
+    const client = Faye.Client(this.apiUrl, { retry: config.retry });
+
+    if (options.ajaxOnly) {
+      client.disable('websocket');
+    }
 
     if (isFunction(options.onQuestionApproved)) {
-      this.client.subscribe('/onQuestionApproved', options.onQuestionApproved);
+      client.subscribe('/onQuestionApproved', options.onQuestionApproved);
     }
 
     if (isFunction(options.onQuestionsReceived)) {
-      this.client.subscribe('/onQuestionsReceived', options.onQuestionsReceived);
+      client.subscribe('/onQuestionsReceived', options.onQuestionsReceived);
     }
 
     if (isFunction(options.onNewQuestionsReceived)) {
-      this.client.subscribe('/onNewQuestionsReceived', options.onNewQuestionsReceived);
+      client.subscribe('/onNewQuestionsReceived', options.onNewQuestionsReceived);
     }
+
+    this.client = client;
   }
 
   sendQuestion({ name, from, message } = {}) {
