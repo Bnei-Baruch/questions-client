4ec349d8ce706faa4e5332fc10ddb8095bdc05c3
Yaniv Elimor <yaniv.elimor@gmail.com>
2016-11-04 09:22:58 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -4,12 +4,11 @@ const client = new QuestionsClient({
   url: 'http://localhost:8000'
 });
 
-// client.client.subsrcibe('/sendQuestion', (a) => alert(a));
-
-client.sendQuestion({
-  name: 'yaniv',
-  from: 'haifa',
-  message: 'hi im testing'
-}).then((data) => console.log(data))
-.catch(error => console.log(error));
-
+client.client.subscribe('/sendQuestion', (a) => console.log(a)).then(() => {
+  client.sendQuestion({
+    name: 'yaniv',
+    from: 'haifa',
+    message: 'hi im testing'
+  }).then(() => console.log('message was send successfuly'))
+  .catch(error => console.log(error));
+});
